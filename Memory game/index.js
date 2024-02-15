347df31f08835ec9be1ba3790d27940a651db80a
Rishi Kashyap <130247285+Rishikashyap007@users.noreply.github.com>
2024-02-15 22:56:21 +0530

    Update index.js

--- Memory game/index.js
+++ Memory game/index.js
@@ -69,10 +69,8 @@ const gameWons= [];
 function createBoard(){
     for (let i=0; i<12 ;i++){
         const card = document.createElement('img')
-        // console.log(card,i) 
         card.setAttribute('src','https://imgs.search.brave.com/aL4pF0d-J8tuXj4iUlU3dHcOUEsMhnP1RZz8OeRuTzE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjM4MjI4OTgwNTkt/NzI3Y2ViZWI4Njcw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TVRKOGZH/SnNZV05ySlRJd1ky/RnlaSHhsYm53d2ZI/d3dmSHg4TUE9PQ')
         card.setAttribute('id',i)
-        //console.log(card);
         gameDisplay[0].appendChild(card);
         card.addEventListener('click', flipcard)
     }
@@ -81,7 +79,6 @@ function createBoard(){
     const cardId = this.getAttribute('id')
     chosencard.push(cardArray[cardId].name)
     cardChosenId.push(cardId)
-    // console.log(chosencard)
     this.setAttribute('src',cardArray[cardId].img)
 
     if(chosencard.length === 2){
@@ -90,9 +87,7 @@ function createBoard(){
  }
 
  function checkMatch(){
-    //console.log("Still searching")
     const cards = document.querySelectorAll('img') 
-    // console.log(cards)
     if(cardChosenId[0] == cardChosenId[1]){
         cards[cardChosenId[0]].setAttribute('src','https://imgs.search.brave.com/aL4pF0d-J8tuXj4iUlU3dHcOUEsMhnP1RZz8OeRuTzE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjM4MjI4OTgwNTkt/NzI3Y2ViZWI4Njcw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TVRKOGZH/SnNZV05ySlRJd1ky/RnlaSHhsYm53d2ZI/d3dmSHg4TUE9PQ')
         cards[cardChosenId[1]].setAttribute('src','https://imgs.search.brave.com/aL4pF0d-J8tuXj4iUlU3dHcOUEsMhnP1RZz8OeRuTzE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjM4MjI4OTgwNTkt/NzI3Y2ViZWI4Njcw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TVRKOGZH/SnNZV05ySlRJd1ky/RnlaSHhsYm53d2ZI/d3dmSHg4TUE9PQ')
